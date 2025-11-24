b2d41707ee455e0f13b789629dc9ddcbebe1c019
kiranspradeep <kiranspradeep2002@gmail.com>
2025-11-24 23:27:11 +0530

    dted files

--- backend/src/modules/shop-files/fileTypes.js
+++
@@ -1,9 +0,0 @@
-export const ALLOWED_FILE_TYPES = [
-  "pharmacy_license",
-  "drug_license",
-  "gst_certificate",
-  "owner_pan",
-  "shop_act_license",
-  "business_registration",
-  "address_proof",
-];
