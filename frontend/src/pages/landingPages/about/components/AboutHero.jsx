58ecec5cadac482443465ce66a63275ca24b4fba
kiranspradeep-yourzerosandones <kiranspradeep@yourzerosandones.com>
2026-05-06 10:51:04 +0530

    to webp

--- frontend/src/pages/landingPages/about/components/AboutHero.jsx
+++ frontend/src/pages/landingPages/about/components/AboutHero.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-import HeroBG from "../../../../assets/images/About-hero.png";
+import HeroBG from "../../../../assets/images/About-hero_result.webp";
 
 // Import PNG icons
 import expIcon from "../../../../assets/icons/exp.png";
