3cb26fe3335a800fcfb09ba9a64f89b82efab00e
asifkfaiz <asifkfaiz@gmail.com>
2026-07-21 14:57:13 +0530

    fix BASE_URL https for production builds

--- cureli-mobile/src/constants/config.ts
+++ cureli-mobile/src/constants/config.ts
@@ -11,7 +11,7 @@ const PROD_BASE_URL = "https://api.curelihealth.com";
 // localhost:5000
 
 export const CONFIG = {
-  BASE_URL: "http://api.curelihealth.com", // ← hardcoded for this build
+  BASE_URL: "https://api.curelihealth.com", // ← hardcoded for this build
   API_TIMEOUT: 15000,
 };
 
