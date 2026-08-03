d625cfce546b8b809afa6df4b516c7bbe33fee32
asifkfaiz <asifkfaiz@gmail.com>
2026-08-03 12:29:55 +0530

    new rider app foundation

--- cureli-delivery/src/constants/storage.ts
+++
@@ -1,9 +0,0 @@
-// src/constants/storage.ts
-export const STORAGE_KEYS = {
-  ACCESS_TOKEN:  'auth.access_token',
-  REFRESH_TOKEN: 'auth.refresh_token',
-  RIDER:         'auth.rider',
-  THEME_PREFERENCE: 'app.theme_preference',
-} as const;
-
-export type StorageKey = (typeof STORAGE_KEYS)[keyof typeof STORAGE_KEYS];
